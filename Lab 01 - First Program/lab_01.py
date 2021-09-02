b2030184fd87a8e4159c9cedc53bbf1177745b97
Maggie Fitzpatrick <maggie.fitzpatrick@my.simpson.edu>
2021-09-02 15:21:44 -0500

    Work on lab 1

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,4 @@
+print("I don't know what to write.")
+print("I guess I'll say that my name is Maggie.")
+print("I have a dog named Bella who is a labradoodle.")
+print("One time, a Buzzfeed quiz told me that I would be a labradoodle if I were a dog too.")
