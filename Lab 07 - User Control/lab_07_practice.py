457d5cc317e3674c13c2317b908fc1bfeac31df2
Maggie Fitzpatrick <maggie.fitzpatrick@my.simpson.edu>
2021-10-28 11:19:48 -0500

    Work on lab 7

--- Lab 07 - User Control/lab_07_practice.py
+++ Lab 07 - User Control/lab_07_practice.py
@@ -2,90 +2,50 @@ import arcade
 
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
-
-class Ball:
-    def __init__(self, position_x, position_y, radius, color):
-
-        # Take the parameters of the init function above,
-        # and create instance variables out of them.
-        self.position_x = position_x
-        self.position_y = position_y
-        self.radius = radius
-        self.color = color
-
-    def draw(self):
-        """ Draw the balls with the instance variables we have. """
-        arcade.draw_circle_filled(self.position_x,
-                                  self.position_y,
-                                  self.radius,
-                                  self.color)
-
-class Background:
-    def __init__(self, x, y):
-
-        # Take the parameters of the init function above,
-        # and create instance variables out of them.
-        self.x = x
-        self.y = y
-
-    def draw_sunset(x, y):
-        """ Draw the sunset """
-
-        # Draw a point at x, y for reference
-        arcade.draw_point(x, y, arcade.color.RED, 10)
-
-        # Draw four different colored arcs that arch across the whole window to look like a sunset.
-        arcade.draw_arc_filled(x, y, 530, 450, arcade.csscolor.RED, 0, 180)
-        arcade.draw_arc_filled(x, y, 500, 420, arcade.csscolor.ORANGE_RED, 0, 180)
-        arcade.draw_arc_filled(x, y, 450, 400, arcade.csscolor.GOLD, 0, 180)
-        arcade.draw_arc_filled(x, y, 350, 350, arcade.color.LEMON, 0, 180)
+MOVEMENT_SPEED = 3
 
 
-    def draw_birds(x, y):
-        """ Draw black birds """
-
-        # Draw a point at x, y for reference
-        arcade.draw_point(x, y, arcade.color.RED, 2)
-
-        # Draw black birds flying past sunset
-        arcade.draw_triangle_filled(x, y, x + 5, y + 5, x - 5, y - 5, arcade.color.BLACK)
-
-
-    def draw_field_and_river(x, y):
-        """ Draw field with river running through it. """
-
-        # Draw a point at x, y for reference
-        arcade.draw_point(x, y, arcade.color.RED, 10)
-
-        # Draw a grassy field with a river running through it.
-        arcade.draw_lrtb_rectangle_filled(0, SCREEN_WIDTH, SCREEN_HEIGHT / 2, 0, arcade.csscolor.LIME_GREEN)
-        arcade.draw_polygon_filled(((x, y),
-                                    (x + 200, y),
-                                    (x + 98, y + 300),
-                                    (x + 102, y + 300),
-                                    ),
-                                   arcade.csscolor.TURQUOISE)
+def draw_sunset(x, y):
+    """ Draw the sunset """
+    # Draw four different colored arcs that arch across the whole window to look like a sunset.
+    arcade.draw_arc_filled(x, y, 530, 450, arcade.csscolor.RED, 0, 180)
+    arcade.draw_arc_filled(x, y, 500, 420, arcade.csscolor.ORANGE_RED, 0, 180)
+    arcade.draw_arc_filled(x, y, 450, 400, arcade.csscolor.GOLD, 0, 180)
+    arcade.draw_arc_filled(x, y, 350, 350, arcade.color.LEMON, 0, 180)
 
 
-    def draw_fence(x, y):
+def draw_birds(x, y):
+    """ Draw black birds """
+    # Draw black birds flying past sunset
+    arcade.draw_triangle_filled(x, y, x + 5, y + 5, x - 5, y - 5, arcade.color.BLACK)
 
-        # Draw a point at x, y for reference
-        arcade.draw_point(x, y, arcade.color.RED, 5)
 
-        # Draw two fences with a collection of blanched almond lines on both sides of the river
-        arcade.draw_line(x, y, x + 230, y, arcade.csscolor.BLANCHED_ALMOND, 10)
-        arcade.draw_line(x, y - 50, x + 230, y - 50, arcade.csscolor.BLANCHED_ALMOND, 10)
-        arcade.draw_line(x + 30, y + 25, x + 30, y - 100, arcade.csscolor.BLANCHED_ALMOND, 10)
-        arcade.draw_line(x + 200, y + 25, x + 200, y - 100, arcade.csscolor.BLANCHED_ALMOND, 10)
+def draw_field_and_river(x, y):
+    """ Draw field with river running through it. """
+    # Draw a grassy field with a river running through it.
+    arcade.draw_lrtb_rectangle_filled(0, SCREEN_WIDTH, SCREEN_HEIGHT / 2, 0, arcade.csscolor.LIME_GREEN)
+    arcade.draw_polygon_filled(((x, y),
+                                (x + 200, y),
+                                (x + 98, y + 300),
+                                (x + 102, y + 300),
+                                ),
+                               arcade.csscolor.TURQUOISE)
 
+
+def draw_fence(x, y):
+    # Draw two fences with a collection of blanched almond lines on both sides of the river
+    arcade.draw_line(x, y, x + 230, y, arcade.csscolor.BLANCHED_ALMOND, 10)
+    arcade.draw_line(x, y - 50, x + 230, y - 50, arcade.csscolor.BLANCHED_ALMOND, 10)
+    arcade.draw_line(x + 30, y + 25, x + 30, y - 100, arcade.csscolor.BLANCHED_ALMOND, 10)
+    arcade.draw_line(x + 200, y + 25, x + 200, y - 100, arcade.csscolor.BLANCHED_ALMOND, 10)
 
 
 class MyGame(arcade.Window):
 
-    def __init__(self, width, height, title):
+    def __init__(self):
 
         # Call the parent class's init function
-        super().__init__(width, height, title)
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Lab 7 - User Control")
 
         # Make the mouse disappear when it is over the window
         self.set_mouse_visible(False)
@@ -93,20 +53,112 @@ class MyGame(arcade.Window):
         arcade.set_background_color(arcade.csscolor.TURQUOISE)
 
         # Create our ball
-        self.ball = Ball(50, 50, 15, arcade.color.BLACK)
+        self.hot_air_balloon = HotAirBalloon(50, 50, 15, 20, 10, arcade.color.PURPLE)
+        self.penguin = Penguin(50, 50, 0, 0, 15, arcade.color.ARMY_GREEN)
 
     def on_draw(self):
         """ Called whenever we need to draw the window. """
         arcade.start_render()
-        self.ball.draw()
+        draw_sunset(300, 300)
+        draw_field_and_river(200, 0)
+        draw_birds(390, 400)
+        draw_fence(20, 250)
+        draw_fence(350, 250)
+        self.hot_air_balloon.draw()
+        self.penguin.draw()
+
+    def update(self, delta_time: float):
+        self.penguin.update()
 
     def on_mouse_motion(self, x, y, dx, dy):
-         """ Updates the ball's position """
-         self.ball.position_x = x
-         self.ball.position_y = y
+        """ Updates the ball's position """
+        self.hot_air_balloon.position_x = x
+        self.hot_air_balloon.position_y = y
+
+    def on_key_press(self, key, modifiers):
+        """ Called whenever the user presses a key. """
+        if key == arcade.key.LEFT:
+            self.penguin.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT:
+            self.penguin.change_x = MOVEMENT_SPEED
+        elif key == arcade.key.UP:
+            self.penguin.change_y = MOVEMENT_SPEED
+        elif key == arcade.key.DOWN:
+            self.penguin.change_y = -MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        """ Called whenever a user releases a key. """
+        if key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.penguin.change_x = 0
+        elif key == arcade.key.UP or key == arcade.key.DOWN:
+            self.penguin.change_y = 0
+
+
+class HotAirBalloon:
+    def __init__(self, position_x, position_y, radius, width, height, color):
+        # Take the parameters of the init function above,
+        # and create instance variables out of them.
+        self.position_x = position_x
+        self.position_y = position_y
+        self.radius = radius
+        self.width = width
+        self.height = height
+        self.color = color
+
+    def draw(self):
+        """ Draw the balls with the instance variables we have. """
+        arcade.draw_ellipse_filled(self.position_x,
+                                   self.position_y,
+                                   30,
+                                   40,
+                                   self.radius,
+                                   self.color)
+
+
+class Penguin:
+    def __init__(self, position_x, position_y, change_x, change_y, radius, color):
+        self.position_x = position_x
+        self.position_y = position_y
+        self.change_x = change_x
+        self.change_y = change_y
+        self.radius = radius
+        self.color = color
+
+    def draw(self):
+        """ Draw the penguin with the instance variables we have. """
+        arcade.draw_circle_filled(self.position_x,
+                                  self.position_y,
+                                  self.radius,
+                                  self.color)
+        arcade.draw_ellipse_filled(self.position_x, self.position_y, 50, 60, arcade.color.BLACK, 0, -1)
+        arcade.draw_ellipse_filled(self.position_x, self.position_y, 30, 40, arcade.color.WHITE, 0, -1)
+        arcade.draw_circle_filled(self.position_x, self.position_y + 40, self.radius + 5, arcade.color.BLACK, 0, -1)
+        arcade.draw_circle_filled(self.position_x, self.position_y + 40, self.radius - 2, arcade.color.WHITE)
+        arcade.draw_circle_filled(self.position_x - 5, self.position_y + 45, self.radius - 12, arcade.color.BLACK)
+        arcade.draw_circle_filled(self.position_x + 5, self.position_y + 45, self.radius - 12, arcade.color.BLACK)
+        arcade.draw_triangle_filled(self.position_x, self.position_y + 40, self.position_x, self.position_y + 30,
+                                    self.position_x + 10, self.position_y + 32, arcade.color.YELLOW_ORANGE)
+
+    def update(self):
+        self.position_x += self.change_x
+        self.position_y += self.change_y
+
+        # See if the ball hit the edge of the screen. If so, change direction
+        if self.position_x < self.radius:
+            self.position_x = self.radius
+
+        if self.position_x > SCREEN_WIDTH - self.radius:
+            self.position_x = SCREEN_WIDTH - self.radius
+
+        if self.position_y < self.radius:
+            self.position_y = self.radius
+
+        if self.position_y > SCREEN_HEIGHT - self.radius:
+            self.position_y = SCREEN_HEIGHT - self.radius
+
 
 def main():
-    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, "Lab 07 - Fitzpatrick")
+    window = MyGame()
     arcade.run()
 
 
