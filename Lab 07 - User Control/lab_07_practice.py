f6439fbdcd7b74e7352a69e0ce923755ddfed83b
Maggie Fitzpatrick <maggie.fitzpatrick@my.simpson.edu>
2021-10-28 15:43:46 -0500

    Work on lab 8

--- Lab 07 - User Control/lab_07_practice.py
+++ Lab 07 - User Control/lab_07_practice.py
@@ -20,64 +20,55 @@ class Ball:
                                   self.radius,
                                   self.color)
 
-class Background:
-    def __init__(self, x, y):
 
-        # Take the parameters of the init function above,
-        # and create instance variables out of them.
-        self.x = x
-        self.y = y
+def draw_sunset(x, y):
+    """ Draw the sunset """
 
-    def draw_sunset(x, y):
-        """ Draw the sunset """
+    # Draw a point at x, y for reference
+    arcade.draw_point(x, y, arcade.color.RED, 10)
 
-        # Draw a point at x, y for reference
-        arcade.draw_point(x, y, arcade.color.RED, 10)
-
-        # Draw four different colored arcs that arch across the whole window to look like a sunset.
-        arcade.draw_arc_filled(x, y, 530, 450, arcade.csscolor.RED, 0, 180)
-        arcade.draw_arc_filled(x, y, 500, 420, arcade.csscolor.ORANGE_RED, 0, 180)
-        arcade.draw_arc_filled(x, y, 450, 400, arcade.csscolor.GOLD, 0, 180)
-        arcade.draw_arc_filled(x, y, 350, 350, arcade.color.LEMON, 0, 180)
+    # Draw four different colored arcs that arch across the whole window to look like a sunset.
+    arcade.draw_arc_filled(x, y, 530, 450, arcade.csscolor.RED, 0, 180)
+    arcade.draw_arc_filled(x, y, 500, 420, arcade.csscolor.ORANGE_RED, 0, 180)
+    arcade.draw_arc_filled(x, y, 450, 400, arcade.csscolor.GOLD, 0, 180)
+    arcade.draw_arc_filled(x, y, 350, 350, arcade.color.LEMON, 0, 180)
 
 
-    def draw_birds(x, y):
-        """ Draw black birds """
+def draw_birds(x, y):
+    """ Draw black birds """
 
-        # Draw a point at x, y for reference
-        arcade.draw_point(x, y, arcade.color.RED, 2)
+    # Draw a point at x, y for reference
+    arcade.draw_point(x, y, arcade.color.RED, 2)
 
-        # Draw black birds flying past sunset
-        arcade.draw_triangle_filled(x, y, x + 5, y + 5, x - 5, y - 5, arcade.color.BLACK)
-
-
-    def draw_field_and_river(x, y):
-        """ Draw field with river running through it. """
-
-        # Draw a point at x, y for reference
-        arcade.draw_point(x, y, arcade.color.RED, 10)
-
-        # Draw a grassy field with a river running through it.
-        arcade.draw_lrtb_rectangle_filled(0, SCREEN_WIDTH, SCREEN_HEIGHT / 2, 0, arcade.csscolor.LIME_GREEN)
-        arcade.draw_polygon_filled(((x, y),
-                                    (x + 200, y),
-                                    (x + 98, y + 300),
-                                    (x + 102, y + 300),
-                                    ),
-                                   arcade.csscolor.TURQUOISE)
+    # Draw black birds flying past sunset
+    arcade.draw_triangle_filled(x, y, x + 5, y + 5, x - 5, y - 5, arcade.color.BLACK)
 
 
-    def draw_fence(x, y):
+def draw_field_and_river(x, y):
+    """ Draw field with river running through it. """
 
-        # Draw a point at x, y for reference
-        arcade.draw_point(x, y, arcade.color.RED, 5)
+    # Draw a point at x, y for reference
+    arcade.draw_point(x, y, arcade.color.RED, 10)
 
-        # Draw two fences with a collection of blanched almond lines on both sides of the river
-        arcade.draw_line(x, y, x + 230, y, arcade.csscolor.BLANCHED_ALMOND, 10)
-        arcade.draw_line(x, y - 50, x + 230, y - 50, arcade.csscolor.BLANCHED_ALMOND, 10)
-        arcade.draw_line(x + 30, y + 25, x + 30, y - 100, arcade.csscolor.BLANCHED_ALMOND, 10)
-        arcade.draw_line(x + 200, y + 25, x + 200, y - 100, arcade.csscolor.BLANCHED_ALMOND, 10)
+    # Draw a grassy field with a river running through it.
+    arcade.draw_lrtb_rectangle_filled(0, SCREEN_WIDTH, SCREEN_HEIGHT / 2, 0, arcade.csscolor.LIME_GREEN)
+    arcade.draw_polygon_filled(((x, y),
+                                (x + 200, y),
+                                (x + 98, y + 300),
+                                (x + 102, y + 300),
+                                ),
+                                arcade.csscolor.TURQUOISE)
 
+def draw_fence(x, y):
+
+    # Draw a point at x, y for reference
+    arcade.draw_point(x, y, arcade.color.RED, 5)
+
+    # Draw two fences with a collection of blanched almond lines on both sides of the river
+    arcade.draw_line(x, y, x + 230, y, arcade.csscolor.BLANCHED_ALMOND, 10)
+    arcade.draw_line(x, y - 50, x + 230, y - 50, arcade.csscolor.BLANCHED_ALMOND, 10)
+    arcade.draw_line(x + 30, y + 25, x + 30, y - 100, arcade.csscolor.BLANCHED_ALMOND, 10)
+    arcade.draw_line(x + 200, y + 25, x + 200, y - 100, arcade.csscolor.BLANCHED_ALMOND, 10)
 
 
 class MyGame(arcade.Window):
@@ -99,6 +90,21 @@ class MyGame(arcade.Window):
         """ Called whenever we need to draw the window. """
         arcade.start_render()
         self.ball.draw()
+        # Draw sunset
+        draw_sunset(300, 300)
+
+        # Draw a field with a river going through it
+        draw_field_and_river(200, 0)
+
+        # Draw black birds at three different places
+        draw_birds(390, 400)
+        draw_birds(395, 405)
+
+        # Draw a fence at two different places.
+        draw_fence(20, 250)
+        draw_fence(350, 250)
+
+        arcade.finish_render()
 
     def on_mouse_motion(self, x, y, dx, dy):
          """ Updates the ball's position """
---
+++ Lab 08 - Sprites/lab_08.py
@@ -0,0 +1,193 @@
+
+import random
+import arcade
+
+# --- Constants ---
+SPRITE_SCALING_PLAYER = 0.01
+SPRITE_SCALING_TREAT = 0.05
+SPRITE_SCALING_COOKIE = 0.03
+
+TREAT_COUNT = 50
+COOKIE_COUNT = 50
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+
+class MyGame(arcade.Window):
+    """ Our custom Window Class"""
+
+    def __init__(self):
+        """ Initializer """
+        # Call the parent class initializer
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Lab 8 - Sprites")
+
+        # Variables that will hold sprite lists
+        self.player_list = None
+        self.treat_list = None
+        self.cookie_list = None
+
+        # Set up the player info
+        self.player_sprite = None
+        self.score = 0
+
+        # Don't show the mouse cursor
+        self.set_mouse_visible(False)
+
+        arcade.set_background_color(arcade.color.AMAZON)
+
+        # Load sound when the game is started
+        self.good_sound = arcade.load_sound("Picked Coin Echo 2.wav")
+        self.bad_sound = arcade.load_sound("ALERT_Error.wav")
+
+    def setup(self):
+        """ Set up the game and initialize the variables. """
+
+        # Sprite lists
+        self.player_list = arcade.SpriteList()
+        self.treat_list = arcade.SpriteList()
+        self.cookie_list = arcade.SpriteList()
+
+        # Score
+        self.score = 0
+
+        # Set up the player
+        # Character image from kenney.nl
+        self.player_sprite = arcade.Sprite("Daco_3805650.png",
+                                           SPRITE_SCALING_PLAYER)
+        self.player_sprite.center_x = 50
+        self.player_sprite.center_y = 50
+        self.player_list.append(self.player_sprite)
+
+        # Create the coins
+        for i in range(TREAT_COUNT):
+
+            # Create the treat instance
+            # Treat image from kenney.nl
+            treat = Treat("kisspng-labrador-retriever-puppy-bone-drawing-clip-art-free-dog-bone-clipart-"
+                          "5aaba1f1a3fb81.0608378215211975536717.png", SPRITE_SCALING_TREAT)
+
+            # Position the coin
+            treat.center_x = random.randrange(SCREEN_WIDTH)
+            treat.center_y = random.randrange(SCREEN_HEIGHT)
+            treat.change_x = random.randrange(-3, 4)
+            treat.change_y = random.randrange(-3, 4)
+
+            # Add the coin to the lists
+            self.treat_list.append(treat)
+
+        # Create the cookies
+        for i in range(COOKIE_COUNT):
+
+            # Create the treat instance
+            # Treat image from kenney.nl
+            cookie = Cookie("NicePng_cookie-png_34513.png", SPRITE_SCALING_COOKIE)
+
+            # Position the coin
+            cookie.center_x = random.randrange(SCREEN_WIDTH)
+            cookie.center_y = random.randrange(SCREEN_HEIGHT)
+
+            # Add the coin to the lists
+            self.cookie_list.append(cookie)
+
+    def on_draw(self):
+        """ Draw everything """
+        arcade.start_render()
+        self.treat_list.draw()
+        self.player_list.draw()
+        self.cookie_list.draw()
+
+        # Put the text on the screen.
+        output = f"Score: {self.score}"
+        arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        """ Handle Mouse Motion """
+
+        # Move the center of the player sprite to match the mouse x, y
+        self.player_sprite.center_x = x
+        self.player_sprite.center_y = y
+
+    def update(self, delta_time):
+        """ Movement and game logic """
+
+        # Call update on all sprites (The sprites don't do much in this
+        # example though.)
+        self.treat_list.update()
+        self.cookie_list.update()
+
+        # Generate a list of all sprites that collided with the player.
+        treats_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
+                                                               self.treat_list)
+        cookies_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
+                                                                self.cookie_list)
+
+        # Loop through each colliding treat, remove it, and add to the score.
+        for treat in treats_hit_list:
+            arcade.play_sound(self.good_sound)
+            treat.remove_from_sprite_lists()
+            self.score += 1
+
+        # Loop through each colliding cookie, remove it, and decrease the score by 1
+        for cookie in cookies_hit_list:
+            arcade.play_sound(self.bad_sound)
+            cookie.remove_from_sprite_lists()
+            self.score -= 1
+
+
+class Treat(arcade.Sprite):
+    """ This class represents the treats on the screen. It is a child class of the arcade library's sprite class."""
+    def reset_pos(self):
+
+        # Resets treat to random spot above the top of the screen
+        self.center_y = random.randrange(SCREEN_HEIGHT + 20,
+                                         SCREEN_HEIGHT + 100)
+        self.center_x = random.randrange(SCREEN_WIDTH)
+
+    def update(self):
+        # Move the coin down screen vertically
+        self.center_y -= 1
+
+        # Reset treats to the top when they go off the screen
+        if self.top < 0:
+            self.reset_pos()
+
+
+class Cookie(arcade.Sprite):
+    """ This class represents the cookies on the screen. It is a child class of the arcade library's sprite class. """
+
+    def __init__(self, filename, sprite_scaling):
+
+        super().__init__(filename, sprite_scaling)
+
+        self.change_x = 0
+        self.change_y = 0
+
+    def update(self):
+        # Move the coin
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        # If we are out-of-bounds, then bounce
+        if self.left < 0:
+            self.change_x *= -1
+
+        if self.right > SCREEN_WIDTH:
+            self.change_y *= -1
+
+        if self.bottom < 0:
+            self.center_y *= -1
+
+        if self.top > SCREEN_HEIGHT:
+            self.center_y *= -1
+
+
+def main():
+    """ Main method """
+    window = MyGame()
+    window.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
